c6bb79b214a3246a3b554aea361c636e71ea6df0
Krystina Nguyen <krystinanguyen06@gmail.com>
2026-05-05 14:47:22 +1000

    Fix logo icon on the navigation bar

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const dropdownMenu = document.querySelector('.dropdown-menu');
 dropdownToggle.addEventListener('click', () => {
     event.stopPropagation(); // Prevent the click event from propagating to the document
     dropdownMenu.classList.toggle('active'); // Toggle the active class to show/hide the dropdown menu
-    const inVisible = dropdownMenu.computedStyleMap.display === 'block';
-    dropdownMenu.computedStyleMap.display = isVisible ? 'none' : 'block'; // Toggle the display property to show/hide the dropdown menu
+    const inVisible = dropdownMenu.style.display === 'block';
+    dropdownMenu.style.display = isVisible ? 'none' : 'block'; // Toggle the display property to show/hide the dropdown menu
 
 // Close dropdown menu when clicking outside of it
     document.addEventListener('click', () => {
-    dropdownMenu.computedStyleMap.displauy = 'none'; // Hide the dropdown menu when clicking outside of it
+    dropdownMenu.style.display = 'none'; // Hide the dropdown menu when clicking outside of it
     dropdownToggle.classList.remove('active'); // Remove the active class from the dropdown toggle button
     });
 });
