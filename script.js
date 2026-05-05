8442a903465a1793ae6d3d5ed77279e462e2ec05
Krystina Nguyen <krystinanguyen06@gmail.com>
2026-05-05 14:16:33 +1000

    Add style and dropdown function to navigation bar

---
+++ script.js
@@ -0,0 +1,17 @@
+// Dropdown toggle list icon animation
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+// Click event
+dropdownToggle.addEventListener('click', () => {
+    event.stopPropagation(); // Prevent the click event from propagating to the document
+    dropdownMenu.classList.toggle('active'); // Toggle the active class to show/hide the dropdown menu
+    const inVisible = dropdownMenu.computedStyleMap.display === 'block';
+    dropdownMenu.computedStyleMap.display = isVisible ? 'none' : 'block'; // Toggle the display property to show/hide the dropdown menu
+
+// Close dropdown menu when clicking outside of it
+    document.addEventListener('click', () => {
+    dropdownMenu.computedStyleMap.displauy = 'none'; // Hide the dropdown menu when clicking outside of it
+    dropdownToggle.classList.remove('active'); // Remove the active class from the dropdown toggle button
+    });
+});
